1d47a4b9edecfd0b331f4cfe4bf23113e013d935
Betoml5 <betoml5@hotmail.com>
2023-01-16 22:47:32 -0600

    Change listen port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require("./app");
 const { connectDB } = require("./database");
+const { config } = require("./config");
 connectDB();
 
 //start server
 
-app.listen(3013, () => {
+app.listen(config.port || 3013, () => {
   console.log("Server is running on port 3013");
 });
